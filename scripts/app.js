10cbc70f3b8600e2e2babdb5fd4ec9050c79c666
Chris Spiegl <chris@chrisspiegl.com>
2022-12-17 15:28:41 +0200

    Current State Sync

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 async function init() {
-  $main = document.getElementById('main')
-  $options = document.getElementById('options')
-  $titleMain = document.getElementById('titleMain')
-  $titleSub = document.getElementById('titleSub')
+  const $main = document.getElementById('main')
+  const $options = document.getElementById('options')
+  const $titleMain = document.getElementById('titleMain')
+  const $titleSub = document.getElementById('titleSub')
+  const $titleEarning = document.getElementById('titleEarning')
 
   const settingsDefault = {
     titleMain: 'GET AFTER IT',
@@ -137,6 +138,29 @@ async function init() {
   $titleSub.addEventListener('dblclick', editTitle('titleSub'))
   $titleSub.addEventListener('blur', blurTitle('titleSub'))
   $titleSub.addEventListener('keydown', preventEnter)
+
+  function startEarningsClock() {
+    const getDaysInMonth = (year, month) => new Date(year, month, 0).getDate()
+
+    let date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1; // 👈️ months are 0-based
+    const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
+
+    const earningsPerMonth = 1_200
+    const earningsPerDay = earningsPerMonth / daysInCurrentMonth
+    const earningsPerSecond = earningsPerDay / 24 / 60 / 60
+
+    setInterval(() => {
+      date = new Date()
+      const secondsToday = date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()
+      const earnedToday = secondsToday * earningsPerSecond
+      // console.log(earningsPerSecond * 60)
+      // console.log(earnedToday)
+      $titleEarning.innerHTML = `$${earnedToday.toFixed(3)}`
+    }, 1000)
+  }
+  startEarningsClock()
 }
 
 init()
